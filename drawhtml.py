4e58f8b058a8c525d1c559b57e7ac843a8c8e3c6
frank1994 <hejun1874@163.com>
2015-03-14 11:50:21 +0800

    done for now

--- drawhtml.py
+++ drawhtml.py
@@ -73,20 +73,12 @@ def drawHTML(dates, rates, tag_count, user, filepath='htmlfile/'):
             count[5], count[4], count[3], count[2], count[1]])
             for tag, count in tag_count.iteritems() if sum(count) > 3 and len(tag.strip()) > 0]
     tags = sorted(tags, key=lambda x: x[1], reverse=True)
-    tag_data = '['
-    tag_data += ','.join("{name:'" + tag[0] + "',value:"+str(tag[1][0])+",color:'#006666'}" for tag in tags)
-    tag_data += ']'
-    # print tag_data
-    ############################### TODO
-    table = ''
-    for tag in tags:
-        table += '<tr>\n'
-        table += '<td>'+tag[0]+'</td>\n'
-        table += '<td>'+str(tag[1][0])[:5]+'</td>\n'
-        for i in xrange(5):
-            table += '<td>'+str(tag[1][2+i])+'</td>\n'
-        table += '</tr>\n'
-    # print table
+    # print tags
+
+    item_nodes = '['
+    item_nodes += ','.join("{name:'" + tag[0] + "',value:"+str(100*float(tag[1][0]))[:5]+"}" for tag in tags)
+    item_nodes += ']'
+    print item_nodes
     content = '''
     <!DOCTYPE html>
 <html>
@@ -106,7 +98,9 @@ def drawHTML(dates, rates, tag_count, user, filepath='htmlfile/'):
             [
                 'echarts',
                 'echarts/chart/bar',  // 使用柱状图就加载bar模块，按需加载
-                'echarts/chart/pie'
+                'echarts/chart/pie',
+                'echarts/chart/line',
+                'echarts/chart/force'
             ],
             function (ec) {
                 // 基于准备好的dom，初始化echarts图表
@@ -116,8 +110,9 @@ def drawHTML(dates, rates, tag_count, user, filepath='htmlfile/'):
                         text:'%(user)s的豆瓣评分',
                         x:'center'
                     },
-                    tooltip: {
-                        show: true
+                    tooltip : {
+                        trigger: 'item',
+                        formatter: "{a} <br/>{b} : {c} ({d}%%)"
                     },
                     legend: {
                         x:'left',
@@ -146,7 +141,7 @@ def drawHTML(dates, rates, tag_count, user, filepath='htmlfile/'):
                     ],
                     series : [
                         {
-                            "name":"评分",
+                            "name":"%(user)s的豆瓣评分",
                             "type":"bar",
                             "data":%(rate_s_data_num)s,
                             itemStyle:{
@@ -218,41 +213,170 @@ def drawHTML(dates, rates, tag_count, user, filepath='htmlfile/'):
                 };
                 pieChart.setOption(pieOption);
 
+                var lineChart = ec.init(document.getElementById('canvasRatingMonth'));
+                var lineOption = {
+                    title: {
+                        text: '%(user)s的观影数量分布',
+                        x: 'center'
+                    },
+                    legend: {
+                        data:['观看电影数'],
+                        x: 'left',
+                    },
+                    toolbox: {
+                        show : true,
+                        feature : {
+                            mark : {show: true},
+                            dataView : {show: true, readOnly: false},
+                            magicType : {show: true, type: ['line', 'bar']},
+                            restore : {show: true},
+                            saveAsImage : {show: true}
+                        }
+                    },
+                    calculable : true,
+                    xAxis : [
+                        {
+                            type : 'category',
+                            boundaryGap : false,
+                            data : %(month_name)s
+                        }
+                    ],
+                    yAxis : [
+                        {
+                            type : 'value',
+                            axisLabel : {
+                                formatter: '{value} 部'
+                            }
+                        }
+                    ],
+                    series: [
+                        {
+                            name: '%(user)s的观看电影数量分布',
+                            type:'line',
+                            symbol:'emptyCircle',
+                            symbolSize: 5,
+                            itemStyle: {
+                                normal: {
+                                    lineStyle: {            // 系列级个性化折线样式，横向渐变描边
+                                        width: 2,
+                                        color: (function (){
+                                            var zrColor = require('zrender/tool/color');
+                                            return zrColor.getLinearGradient(
+                                                0, 0, 1000, 0,
+                                                [[0, 'rgba(255,0,0,0.8)'],[0.8, 'rgba(255,255,0,0.8)']]
+                                            )
+                                        })(),
+                                        shadowColor : 'rgba(0,0,0,0.5)',
+                                        shadowBlur: 10,
+                                        shadowOffsetX: 8,
+                                        shadowOffsetY: 8
+                                    }
+                                },
+                                emphasis : {
+                                    label : {show: true}
+                                }
+                            },
+                            markLine : {
+                                data : [
+                                    {type : 'average', name: '平均值'}
+                                ]
+                            },
+                            data: %(month_count)s
+                        }
+                    ]
+                };
+                lineChart.setOption(lineOption);
 
+                var forceChart = ec.init(document.getElementById('canvasFavItem'));
+                var forceOption = {
+                    title : {
+                        text: '%(user)s的标签',
+                        x:'center'
+                    },
+                    tooltip : {
+                        trigger: 'item',
+                        formatter: "{a} <br/>{b} : {c}%%"
+                    },
+                    toolbox: {
+                        show : true,
+                        feature : {
+                            restore : {show: true},
+                            magicType: {show: true, type: ['force', 'chord']},
+                            saveAsImage : {show: true}
+                        }
+                    },
+                    legend: {
+                        x: 'left',
+                        data:['标签']
+                    },
+                    series : [
+                        {
+                            type:'force',
+                            name : "标签4，5星占比",
+                            ribbonType: false,
+                            itemStyle: {
+                                normal: {
+                                    label: {
+                                        show: true,
+                                        textStyle: {
+                                            color: '#333'
+                                        }
+                                    },
+                                    nodeStyle : {
+                                        brushType : 'both',
+                                        borderColor : 'rgba(255,215,0,0.4)',
+                                        borderWidth : 1
+                                    }
+                                },
+                                emphasis: {
+                                    label: {
+                                        show: false
+                                        // textStyle: null      // 默认使用全局文本样式，详见TEXTSTYLE
+                                    },
+                                    nodeStyle : {
+                                        //r: 30
+                                    }
+                                }
+                            },
+                            draggable: 'true',
+                            useWorker: false,
+                            minRadius : 15,
+                            maxRadius : 30,
+                            gravity: 2.2,
+                            scaling: 1.1,
+                            roam: 'move',
+                            links:[],
+                            nodes: %(item_nodes)s
+                        }
+                    ]
+                };
+                forceChart.setOption(forceOption);
             }
         );
             </script>
     </head>
     <body>
-        <div id='canvasRatingBar' style="height:400px; width:800px;" ></div>
-        <br><br><br><br><br><br><br><br>
-        <div id='canvasRatingPie' style="height:400px; width:800px;></div>
-        <div id='canvasRatingMonth' style="height:400px; width:800px;></div>
-        <h4>%(user)s喜欢的标签</h4>
-        <table border="1">
-        <tr>
-          <td>标签</td>
-          <td>4，5星占比</td>
-          <td>5星</td>
-          <td>4星</td>
-          <td>3星</td>
-          <td>2星</td>
-          <td>1星</td>
-        </tr>
-        %(table)s
-        </table>
+        <div id='canvasRatingBar' style="height:500px; width:1000px;" ></div>
+        <br><br><br><br><br>
+        <div id='canvasRatingPie' style="height:500px; width:1000px;"></div>
+        <br><br><br><br><br>
+        <div id='canvasRatingMonth' style="height:500px; width:1000px;"></div>
+        <br><br><br><br><br>
+        <div id='canvasFavItem' style="height:800px; width:1000px;"></div>
     </body>
 </html>
     ''' % {
         'user': user,
         'rate_s_data_num': str(rate_s_data_num),
         'rates_s': str(rates_s),
-        'table': table
+        'month_count': str(month_count),
+        'month_name': str(month_name),
+        'item_nodes': str(item_nodes)
     }
-    print "rates_s:", rates_s
-    print "month_count:", str(month_count)    # 用来画折线图
-    print "month_name:", str(month_name)      # 用来画折线图
-    print "monthwidth:", str(len(month_name) * 20)
+    # print "rates_s:", rates_s
+    # print "month_count:", str(month_count)    # 用来画折线图
+    # print "month_name:", str(month_name)      # 用来画折线图
+    # print "monthwidth:", str(len(month_name) * 20)
 
     with open(os.path.join(filepath, user+'.html'), 'w') as output:
         output.write(content)
